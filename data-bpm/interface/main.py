f4fd90567d08e5af1878253deba48e6844a27fce
Shubhi Jain <email.shubhi.jain@gmail.com>
2024-03-04 22:44:48 +0100

    Skeleton for the model package

---
+++ data-bpm/interface/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import os.path as Path
+from data-bpm.ml-logic.data import get_data
+
+def preprocess():
+
+    ## Load the data
+    get_data()
+    clean_data()
+
+
+
+def train():
+    pass
+
+def evaluate():
+    pass
+
+def pred():
+    pass
+
+if __name__ == '__main__':
+    preprocess()
+    train()
+    evaluate()
+    pred()
---
+++ data-bpm/ml-logic/data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os.path as Path
+
+def clean_data():
+    '''
+    Clean the data, merge the three files and returns a single datframe
+    '''
+    return pd.DataFrame
+
+def get_data():
+    # Get Local Data
+    data_events_ppl = pd.read_csv(Path.join("..", "raw_data", "240304 BPM Events list people  - ALL __.csv"))
+    data_scraped = pd.read_csv(Path.join("..","raw_data", "../raw_data/result.csv"))
+    data_events_series = pd.read_csv(Path.join("..","raw_data", "BPM Events list people"))
+
+    clean_data()
+
+def load_data_to_bq(
+        data: pd.DataFrame,
+        gcp_project:str,
+        bq_dataset:str,
+        table: str,
+        truncate: bool
+    ) -> None:
+
+    """
+    - Save the DataFrame to BigQuery
+    - Empty the table beforehand if `truncate` is True, append otherwise
+    """

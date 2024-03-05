c26c0d2f6834f410eb3b490e6445fb7982e608e0
Dominic Hodal <dhodal@proton.me>
2024-03-05 10:00:01 +0100

    updated api and added my data exploration notebook from yesterday

--- data-bpm/api/bpm.py
+++ data-bpm/api/bpm.py
@@ -1,15 +1,34 @@
+import pandas as pdf
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
-'''
-@app.get('/predict')
-def suggestion(features):
-    # Compute attendee course suggestion `
-    course = 1 # compute course selection -> to be done
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
-    return {'course': course}
-'''
+
+# app.state.model = load_model()
+
+# @app.get('/predict')
+# def suggestion(features):
+#     # Compute attendee course suggestion 
+#     model = app.state.model`
+#     course = 1 # compute course selection -> to be done
+    # model = app.state.model
+    # assert model is not None
+
+    # X_processed = preprocess_features(X_pred)
+    # y_pred = model.predict(X_processed)
+
+#     return {'course': y_pred}
 
 
 

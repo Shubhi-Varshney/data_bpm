e3d6d25ddb7c43d8bca9fad473340ffee2030978
Shubhi Jain <email.shubhi.jain@gmail.com>
2024-03-07 12:28:12 +0100

    Added pipeline

--- data_bpm/interface/main.py
+++ data_bpm/interface/main.py
@@ -2,17 +2,21 @@ import pandas as pd
 import numpy as np
 import os.path as Path
 from data_bpm.ml_logic.data import get_data, clean_data
+from data_bpm.ml_logic.preprocessor import preprocess_features
 
 def preprocess():
     ## Load the data
 
-    data_for_ml, data_for_analytics = get_data()
-    print(data_for_ml)
-    print(data_for_ml.info())
-    print(data_for_analytics)
-    print(data_for_analytics.info())
-    data_for_analytics.to_csv('raw_data/data_for_analytics.csv')
-    # clean_data()
+    #return " @@@@ Hello "
+    # data_for_ml, data_for_analytics = get_data()
+    # print(data_for_ml)
+    # print(data_for_ml.info())
+    # print(data_for_analytics)
+    # print(data_for_analytics.info())
+    # data_for_analytics.to_csv('raw_data/data_for_analytics.csv')
+    data_for_ml = pd.read_csv('raw_data/data_for_ml.csv')
+
+    preprocess_features(data_for_ml)
 
 def train():
     pass
